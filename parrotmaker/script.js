b172946f95fa85afde09c69f0778a4adf80f508e
BuildTools <unconfigured@null.spigotmc.org>
2021-11-25 00:08:13 -0600

    Added new page

---
+++ parrotmaker/script.js
@@ -0,0 +1,44 @@
+let input;
+let maxVal=8;
+function Main() {
+
+    function tDist(x1, y1, x2, y2) {
+        return (Math.abs(x2 - x1) + Math.abs(y2 - y1))
+    }
+    inarea = document.getElementById("inarea")
+    input = inarea.value;
+    console.log(input)
+    input = input.split("\n").map(x => x.split("\t"));
+    output = [];
+    for (let i = 0; i < input.length; i++) {
+        temp = [];
+        for (let j = 0; j < input[i].length; j++) {
+            if (input[i][j] === '') {
+                let minDist = maxVal-1;
+
+                for (let k = 0; k < input.length; k++) {
+                    for (let l = 0; l < input[k].length; l++) {
+                        if (input[k][l] !== '') {
+                            if ([d = tDist(i, j, k, l)] < minDist) {//set tDist to d instead of calculating twice
+                                minDist = d;
+                            }
+                        }
+                    }
+                }
+                temp[j] = minDist
+            }
+            else {
+                temp[j] = 0;
+            }
+            output[i] = temp;
+        }
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input[i].length; j++) {
+            let o = document.getElementById('invert').checked?maxVal-output[i][j]:output[i][j]+1;
+            output[i][j] = o > 0 && o < 8 ? ":parrotwave" + o + ":" : ":parrotsleep:"
+        }
+    }
+inarea.value='.\n'+output.map(e => e.join('\t')).join('\n');
+}
